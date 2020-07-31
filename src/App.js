cab8fd561432cc3e21879377613d831146154225
anami823 <anami823@gmail.com>
2020-07-30 21:00:41 -0700

    Configure particles.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import Particles from "react-particles-js";
 
 import "./App.css";
 
@@ -28,6 +29,46 @@ function App() {
 
   return (
     <>
+      <div style={{ backgroundColor: "#000000", position: "absolute", zIndex: "-1" }}>
+        <Particles
+          params={{
+            particles: {
+              color: "rgba(255, 255, 0, 1)",
+              number: {
+                value: 40,
+                density: {
+                  enable: false
+                }
+              },
+              size: {
+                value: 3,
+                random: true
+              },
+              move: {
+                direction: "top",
+                out_mode: "out",
+                straight: "true"
+              },
+              line_linked: {
+                enable: false
+              }
+            },
+            interactivity: {
+              events: {
+                onclick: {
+                  enable: true,
+                  mode: "remove"
+                }
+              },
+              modes: {
+                remove: {
+                  particles_nb: 10
+                }
+              }
+            }
+          }}
+        />
+      </div>
       <div>{character.name}</div>
       <div>{vehicle.name}</div>
     </>
